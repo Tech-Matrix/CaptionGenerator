5ef27204f800eac58b2b5109be4627dfe9ded56f
legbing <spoorthikalkunte@gmail.com>
2021-09-19 19:12:42 +0530

    tested out with higher number of epochs in training

--- Back-end/testing_caption_generator.py
+++ Back-end/testing_caption_generator.py
@@ -57,7 +57,7 @@ def generate_desc(model, tokenizer, photo, max_length):
 #path = 'Flicker8k_Dataset/111537222_07e56d5a30.jpg'
 max_length = 32
 tokenizer = load(open("tokenizer.p","rb"))
-model = load_model('models/model_8.h5')
+model = load_model('models\model_9.h5')
 xception_model = Xception(include_top=False, pooling="avg")
 
 photo = extract_features(img_path, xception_model)
